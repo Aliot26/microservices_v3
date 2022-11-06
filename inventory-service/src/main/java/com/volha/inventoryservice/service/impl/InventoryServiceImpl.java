5cc18dd1ed0f9a9cdcbdb28909f9e66283f73f1f
Aliot26 <olgakochanowa@gmail.com>
2022-11-06 11:23:34 +0100

    Added Circuit breaker and when response is waiting more than 3 s.

--- inventory-service/src/main/java/com/volha/inventoryservice/service/impl/InventoryServiceImpl.java
+++ inventory-service/src/main/java/com/volha/inventoryservice/service/impl/InventoryServiceImpl.java
@@ -4,6 +4,8 @@ import com.volha.inventoryservice.dto.InventoryResponse;
 import com.volha.inventoryservice.repository.InventoryRepository;
 import com.volha.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryServiceImpl implements InventoryService {
     private final InventoryRepository inventoryRepository;
 
 
     @Transactional(readOnly = true)
+    @SneakyThrows //don't us it in production
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory -> InventoryResponse.builder()
                             .skuCode(inventory.getSkuCode())
